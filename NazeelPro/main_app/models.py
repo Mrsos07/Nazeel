e4e8e537ab19d4baf69f3f906a9a1523d809bd35
saud <sasud000@gmail.com>
2023-06-14 13:37:26 +0300

    added hotel model

--- NazeelPro/main_app/models.py
+++ NazeelPro/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images/", default="images/default.jpg")
