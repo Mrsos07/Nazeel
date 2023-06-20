8ba3fa5b318d1b52ac78ec04a29088072575c8ad
SulimanBinM <sbinmohammed_1@icloud.com>
2023-06-20 12:07:05 +0300

    theres an issue in delete items

--- NazeelPro/main_app/urls.py
+++ NazeelPro/main_app/urls.py
@@ -8,7 +8,11 @@ urlpatterns = [
     path("history/",views.history,name="history"),
     path("about/",views.about,name="about"),
     path("order/<main_services_id>/",views.order,name="order"),
-    path("maps/",views.maps,name="maps")
+    path("maps/", views.maps, name="maps"),
+    path("add/review/", views.add_review, name="add_review"),
+    path("chatbot/",views.chatbot,name='chatbot'),
+    path('logout/', views.logout_page, name='logout_page'),
 ]
+
 
 
