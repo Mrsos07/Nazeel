13a1c219ce971b7d550f6f937691c5c74039039a
saud <sasud000@gmail.com>
2023-06-20 18:38:22 +0300

    replace

--- NazeelPro/main_app/urls.py
+++ NazeelPro/main_app/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("add/review/", views.add_review, name="add_review"),
     path("chatbot/",views.chatbot,name='chatbot'),
     path('logout/', views.logout_page, name='logout_page'),
+
 ]
 
 
--- NazeelPro/service_app/urls.py
+++ NazeelPro/service_app/urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     path("menu/<main_services_id>/",views.menu,name='menu'),
     path("add/",views.add_service,name='add_service'),
     path("edit/items/<main_services_id>/",views.edit_items,name='edit_items'),
-    path("order/request/<main_services_id>/",views.order_request,name='order_request'),
+    path("order/request/", views.submit_request, name='order_request'),
     path("active/order/<main_services_id>/",views.active_order,name='active_order'),
     path("delete/item/<item_id>/",views.delete_items,name='delete_items'),
     path("edit/<main_services_id>/",views.edit_main_service,name='edit_main_service'),
     path("delete/service/<main_services_id>/",views.delete_service,name='delete_service'),
+
 ]
